5c69cff87bf9e019416d0f7d3ab8f3d1a3052ca9
ailan <ailan@ailan>
2017-05-11 18:05:02 +0800

    Chapter 6 finished

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,5 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^main/', include('main.urls', namespace='main')),
+    url(r'^article/', include('article.urls', namespace='article')),
     url(r'^.*', include('main.urls')),
+    
 ]
